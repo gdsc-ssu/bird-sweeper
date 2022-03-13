570e5c7473ed8a0c2ce6ff06d460517a3094e91d
owjs3901 <owjs3901@naver.com>
2022-03-14 00:14:17 +0900

    Refactor bottomsheet

--- flutter-app/lib/pages/manage_page.dart
+++ flutter-app/lib/pages/manage_page.dart
@@ -31,35 +31,66 @@ class _ManagePageState extends State<ManagePage> {
           DraggableScrollableSheet(
             initialChildSize: 0.30,
             minChildSize: 0.2,
-            builder: (context, scrollController) => Material(
-              color: Colors.white,
-              child: ListView.builder(
-                controller: scrollController,
-                itemBuilder: (context, index) => InkWell(
-                  onTap: () => print("hello"),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 16),
-                    child: Row(
-                      children: [
-                        ClipOval(
-                          child: Container(
-                            width: 32,
-                            height: 32,
-                            color: Colors.red,
+            builder: (context, scrollController) => SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                children: [
+                  Container(
+                      height: 48,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(24)),
+                      )),
+                  Container(
+                    color: Colors.white,
+                    child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Divider(
+                          height: 0,
+                        );
+                      },
+                      controller: scrollController,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) => Material(
+                        color: Colors.white,
+                        child: InkWell(
+                          onTap: () => print("hello"),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 16),
+                            child: Row(
+                              children: [
+                                ClipOval(
+                                  child: Container(
+                                    width: 32,
+                                    height: 32,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 16,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      "aaa",
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    Text("aaa", style: TextStyle(fontSize: 12))
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Column(
-                          children: [Text("aaa"), Text("aaa")],
-                        )
-                      ],
+                      ),
+                      itemCount: 25,
                     ),
                   ),
-                ),
-                itemCount: 25,
+                ],
               ),
             ),
           )
