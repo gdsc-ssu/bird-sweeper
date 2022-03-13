a21fb5fd7bdca9ca51a8b44d9742fe094a05bd4c
owjs3901 <owjs3901@naver.com>
2022-03-14 01:34:12 +0900

    Implement DetailPage

--- flutter-app/lib/pages/detail_page.dart
+++ flutter-app/lib/pages/detail_page.dart
@@ -19,6 +19,99 @@ class _DetailPageState extends State<DetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 24, bottom: 16),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Need to be replaced",
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Color(0xFF292927),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Renewaled : 10 min ago",
+                          style:
+                              TextStyle(fontSize: 14, color: Color(0xFF292927)),
+                        ),
+                        Padding(padding: EdgeInsets.all(24)),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Visits",
+                                  style: TextStyle(
+                                      color: Color(0xFF858580), fontSize: 14),
+                                ),
+                                Text(
+                                  "14",
+                                  style: TextStyle(
+                                      color: Color(0xFF292927), fontSize: 16),
+                                )
+                              ],
+                            )),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Visits",
+                                  style: TextStyle(
+                                      color: Color(0xFF858580), fontSize: 14),
+                                ),
+                                Text(
+                                  "14",
+                                  style: TextStyle(
+                                      color: Color(0xFF292927), fontSize: 16),
+                                )
+                              ],
+                            )),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Visits",
+                                  style: TextStyle(
+                                      color: Color(0xFF858580), fontSize: 14),
+                                ),
+                                Text(
+                                  "14",
+                                  style: TextStyle(
+                                      color: Color(0xFF292927), fontSize: 16),
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: const Color(0xFFF6F6F8),
+    );
   }
 }
