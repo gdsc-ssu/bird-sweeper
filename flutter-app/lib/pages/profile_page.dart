b4aef12d5b8c346489fd0651bbdd4a7dfa15eb35
owjs3901 <owjs3901@naver.com>
2022-03-14 00:36:30 +0900

    Implement profile

--- flutter-app/lib/pages/profile_page.dart
+++ flutter-app/lib/pages/profile_page.dart
@@ -12,11 +12,49 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Garbage collecte12d",
-            style: TextStyle(color: Color(0xFF858580), fontSize: 14))
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(right: 24, left: 24, top: 96),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("관리자",
+              style: TextStyle(color: Color(0xFF858580), fontSize: 14)),
+          Text("철수철구 김철수",
+              style: TextStyle(
+                  color: Color(0xFF292927),
+                  fontSize: 48,
+                  fontWeight: FontWeight.w800)),
+          SizedBox(
+            height: 44,
+          ),
+          Wrap(
+            runSpacing: 20,
+            children: [
+              '담당 지역',
+              '담당 기기',
+              '어플리케이션 정보',
+            ]
+                .map((e) => Row(
+              children: [
+                Text(
+                  e,
+                  style: TextStyle(
+                      color: Color(0xFF858580),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500),
+                ),
+                Expanded(child: SizedBox.shrink()),
+                Text("ww",
+                    style: TextStyle(
+                        color: Color(0xFF292927),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600))
+              ],
+            ))
+                .toList(growable: false),
+          )
+        ],
+      ),
     );
   }
 }
--- flutter-app/lib/pages/report_page.dart
+++ flutter-app/lib/pages/report_page.dart
@@ -12,57 +12,55 @@ class _ReportPageState extends State<ReportPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Padding(
-        padding: const EdgeInsets.only(right: 24, left: 24, top: 96),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Garbage collected",
-                style: TextStyle(color: Color(0xFF858580), fontSize: 14)),
-            Text("190.82t",
-                style: TextStyle(
-                    color: Color(0xFF292927),
-                    fontSize: 48,
-                    fontWeight: FontWeight.w800)),
-            SizedBox(
-              height: 8,
-            ),
-            Text("with Bird Sweeper",
-                style: TextStyle(color: Color(0xFF858580), fontSize: 14)),
-            SizedBox(
-              height: 44,
-            ),
-            Wrap(
-              runSpacing: 20,
-              children: [
-                'Visits',
-                'Animal Type',
-                'Effective date',
-                'Installed device',
-                'Number of keepers'
-              ]
-                  .map((e) => Row(
-                        children: [
-                          Text(
-                            e,
+    return Padding(
+      padding: const EdgeInsets.only(right: 24, left: 24, top: 96),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Garbage collected",
+              style: TextStyle(color: Color(0xFF858580), fontSize: 14)),
+          Text("190.82t",
+              style: TextStyle(
+                  color: Color(0xFF292927),
+                  fontSize: 48,
+                  fontWeight: FontWeight.w800)),
+          SizedBox(
+            height: 8,
+          ),
+          Text("with Bird Sweeper",
+              style: TextStyle(color: Color(0xFF858580), fontSize: 14)),
+          SizedBox(
+            height: 44,
+          ),
+          Wrap(
+            runSpacing: 20,
+            children: [
+              'Visits',
+              'Animal Type',
+              'Effective date',
+              'Installed device',
+              'Number of keepers'
+            ]
+                .map((e) => Row(
+                      children: [
+                        Text(
+                          e,
+                          style: TextStyle(
+                              color: Color(0xFF858580),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Expanded(child: SizedBox.shrink()),
+                        Text("ww",
                             style: TextStyle(
-                                color: Color(0xFF858580),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Expanded(child: SizedBox.shrink()),
-                          Text("ww",
-                              style: TextStyle(
-                                  color: Color(0xFF292927),
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600))
-                        ],
-                      ))
-                  .toList(growable: false),
-            )
-          ],
-        ),
+                                color: Color(0xFF292927),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600))
+                      ],
+                    ))
+                .toList(growable: false),
+          )
+        ],
       ),
     );
   }
